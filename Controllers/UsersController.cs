b5a863ade1ebda1b7ab22655dc34a1aaa9de5b9a
john o'keeffe <jokeeffe09@gmail.com>
2018-03-26 21:28:50 +0100

    Display UserPermissions in User details view

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -24,6 +24,32 @@ namespace StoreApp.Controllers
             return View(await _context.Users.ToListAsync());
         }
 
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .Include(s => s.UserPermissions)
+                    .ThenInclude(u => u.Permission)
+                .AsNoTracking()
+                .SingleOrDefaultAsync(m => m.ID == id);
+
+            //Console.WriteLine(user);
+            
+            // var user = await _context.Users
+            //     .SingleOrDefaultAsync(m = m.ID == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
